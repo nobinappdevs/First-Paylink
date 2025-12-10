69660db035aa7b4310a48a45bce7ea91707ff797
nobinappdevs <nobin@appdevs.team>
2025-12-10 12:47:22 +0600

    layout redesign

--- src/components/dashboard/dashboardHome/DashboardHome.jsx
+++ src/components/dashboard/dashboardHome/DashboardHome.jsx
@@ -7,11 +7,11 @@ const DashboardHome = () => {
   return (
     <div className="2xl:pr-6">
       <Navbar />
-      <div className="grid grid-cols-1 2xl:gap-x-8 xl:gap-x-4 gap-2.5 lg:grid-cols-6">
-        <div className="xl:col-span-4 col-span-6">
+      <div className="grid grid-cols-1 2xl:gap-x-8 xl:gap-x-4 gap-2.5 xl:grid-cols-7 2xl:grid-cols-6">
+        <div className="xl:col-span-4 2xl:col-span-4 col-span-6">
           <DashboardHomeLeft />
         </div>
-        <div className="xl:col-span-2 col-span-6">
+        <div className="xl:col-span-3 2xl:col-span-2 col-span-6">
           <DashboardHomeRight />
         </div>
       </div>
